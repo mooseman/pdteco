faef7fb4434d799e9351f2750e7b06a8b7e0d983
Andy Elvey <andy@obsidian.(none)>
2010-01-16 15:34:32 +1300

    First commit.

---
+++ README.txt
@@ -0,0 +1,24 @@
+
+ **** README -  pdteco   **** '' 
+
+#  Acknowledgement - Very many thanks to the developers of the 
+#  public-domain "eel" implementation of TECO, available here - 
+#  http://www.ibiblio.org/pub/academic/computer-science/history/pdp-11/teco/eel/
+#  This Python implementation would have been much more difficult if that 
+#  version had not been available. 
+
+This is a public-domain Python implementation of the core functionality 
+of the wonderful old TECO editor.  
+
+TECO (in its various implementations) was (and is) an **extremely powerful** 
+tool for the editing of text.  It was, in fact, the predecessor of the famous Emacs
+editor.   
+
+This implementation is likely to be but the palest shadow of the original. 
+Nevertheless, it should be fun to do, and hopefully fun to use. 
+Who knows, it may even (one day) be useful.....  :)  
+
+pdteco is released to the public domain. 
+"Share and enjoy......"  ;)   
+
+
---
+++ pdteco.py
@@ -0,0 +1,50 @@
+
+#  pdteco.py  
+#  A public-domain Python implementation of the core commands of TECO. 
+
+#  This code is released to the public domain. 
+#  "Share and enjoy....."  ;)  
+
+#  Acknowledgement - Very many thanks to the developers of the 
+#  public-domain "eel" implementation of TECO, available here - 
+#  http://www.ibiblio.org/pub/academic/computer-science/history/pdp-11/teco/eel/
+#  This Python implementation would have been much more difficult if that 
+#  version had not been available. 
+
+
+#  *** To do......  *** 
+#  We need to implement the following types of commands - 
+#  a) The eight basic Teco functions 
+#        - DOT (current value of POINT)
+#        - nC - Move POINT \T{n} characters forward. 
+#        - nD - Delete \T{n} characters. 
+#        - Istring\A{ESC} - Insert text. 
+#        - nJ - Move POINT to absolute position \T{n} 
+#        - m,nK - Kill a range of characters. 
+#        - Sstring\A{ESC} - Search for a string. 
+#        - Z - Current buffer size.  
+#  b) Line-oriented commands - 
+#        - nL - Move to beginning of $\T{n}^{th}$ line from \POINT{}. 
+#        - nK - Kill from point  to beginning of $\T{n}^{th}$ following
+#               line. 
+#  c) Looping -  
+#        - n< - Begin \T{n}-iteration loop. 
+#        - >  - End loop. 
+#        - n; - Exit loop if $\T{n} \geq 0$.  
+#  d) Conditionals -  
+#        - n"x - ( To be completed..... )      
+#  e) "Q-registers", to store results. 
+#  f) Conversion functions, from numbers to strings and vice versa.  
+    
+
+class teco(object): 
+   def __init__(self): 
+      pass 
+      
+      
+
+
+
+  
+
+
