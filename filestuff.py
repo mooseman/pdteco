f663f2277cbae2fabfb09a1cf557f7508a1c13e8
Andy Elvey <andy@obsidian.(none)>
2010-01-17 12:20:11 +1300

    More work.

--- filestuff.py
+++ filestuff.py
@@ -2,6 +2,9 @@
 
 #  filestuff.py 
 #  Playing around with f.tell(), f.seek() and so on. 
+#  With f.seek(x, n) - n=0 measures from the start of the file. 
+#  n=1 measures from the current file position. n=2 measures from the 
+#  end of the file. 
 
 # Open a file 
 myfile = open("myfile.txt", 'r') 
@@ -24,15 +27,20 @@ stuff = myfile.read(7)
 # Print the data 
 print stuff 
 
+# Reset "stuff" to missing. 
+stuff = ""
+
+# Move the pointer to the start of the file. 
+myfile.seek(0, 0) 
+
+# Read a few bytes from the file into our string. 
+stuff = myfile.read(10) 
+
+# Print the data 
+print stuff 
+
 # Close the file 
 myfile.close() 
-
-
-
-
-
-
-
 
 
 
